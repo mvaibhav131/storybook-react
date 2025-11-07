e4028630b284763a9cb334f5122b90947a3b33f7
Vaibhav More <98808183+mvaibhav131@users.noreply.github.com>
2025-11-07 11:28:37 +0530

    Fix syntax error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -8,6 +9,6 @@ const nextConfig = {
       },
     ],
   },
-}
+};
 
 export default nextConfig
